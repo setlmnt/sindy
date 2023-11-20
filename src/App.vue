b5fc03d8837bad9ce686bd0674f30188a0956600
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-20 16:21:48 -0300

    :sparkles: 	feat Update theme toggle functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div data-theme="dark" class="flex">
-    <Navbar />
+  <div :data-theme="currentTheme" class="flex h-screen">
+    <Navbar @theme-toggle="toggleTheme" />
     <router-view></router-view>
   </div>
 </template>
+
 <script>
 import Navbar from './components/Navbar.vue'
+
 export default {
   name: 'App',
   components: {
     Navbar
+  },
+  data() {
+    return {
+      currentTheme: 'dark'
+    };
+  },
+  methods: {
+    toggleTheme() {
+      // Alterna entre 'dark' e 'light' (ou outros temas que você tenha)
+      this.currentTheme = this.currentTheme === 'dark' ? 'light' : 'dark';
+    }
   }
 }
 </script>
-
-  
