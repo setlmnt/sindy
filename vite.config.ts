8730aca1b72fb437525897c50f5bbafc6dc71572
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-20 00:04:35 -0300

    Add server host configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '192.168.1.102',
+  },
   plugins: [
     vue(),
     electron({
