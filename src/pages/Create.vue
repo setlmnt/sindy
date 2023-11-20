22427bf1c8652e97267ba2e1f2c2ef171e6f1f06
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-20 00:04:06 -0300

    :recycle: Refactor image selection and camera

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -4,34 +4,23 @@
         <form class="w-3/4  mx-auto  border-2 border-secondary rounded-2xl p-8" @submit.prevent="submitForm">
             <div class="w-full flex">
                 <div class="w-1/2 mr-4">
-                    <div>
-                        <!-- Input para escolher entre abrir a câmera ou escolher do computador -->
-                        <input type="file" @change="handleFileInput" accept="image/*">
+                    <div ref="dropArea"
+                        class="border-2 border-dashed border-accent p-8 w-64 text-center cursor-pointer relative"
+                        @dragover.prevent @dragenter="addDragOverClass" @dragleave="removeDragOverClass" @drop="handleDrop">
+                        <span class="block mb-4">Arraste e solte ou clique para escolher uma imagem</span>
+                        <button @click="openFileInput" class="btn btn-primary w-full">Escolher Arquivo</button>
+                        <button @click="openCamera" class="btn btn-secondary w-full mt-2">Abrir Câmera</button>
 
-                        <!-- Botão para abrir a câmera -->
-                        <button @click="openCamera">Abrir Câmera</button>
-
-                        <!-- Modal para mostrar a câmera -->
-                        <modal v-if="showCameraModal" @close="closeCameraModal">
-                            <div>
-                                <camera :resolution="{ width: 375, height: 600 }" ref="camera" autoplay></camera>
-                                <button @click="takeSnapshot">Capturar Foto</button>
-                                <div v-if="snapshotUrl">
-                                    <img :src="snapshotUrl" alt="Snapshot">
-                                </div>
-                            </div>
-                        </modal>
-
-                        <!-- Modal para mostrar a imagem do computador -->
-                        <modal v-if="showImageModal" @close="closeImageModal">
-                            <div>
-                                <input type="file" @change="handleFileInput" accept="image/*">
-                                <div v-if="selectedImageUrl">
-                                    <img :src="selectedImageUrl" alt="Selected Image">
-                                </div>
-                            </div>
-                        </modal>
+                        <!-- Exibir a imagem da câmera ou arquivo selecionado -->
+                        <div v-if="isCameraActive || selectedImage" class="absolute top-0 left-0 w-full h-full">
+                            <img v-if="selectedImage" :src="selectedImage" alt="Imagem Selecionada"
+                                class="w-full h-full object-cover" />
+                            <video v-if="isCameraActive" ref="cameraFeed" width="100%" height="100%" autoplay></video>
+                            <Button @click="captureFromCamera, clearSelection" v-if="isCameraActive">Capturar</Button>
+                        </div>
+                        <input type="hidden" name="imagem" :value="selectedImage" />
                     </div>
+
                     <div>
                         <label class="label">
                             <span class="label-text">Nome</span>
@@ -138,21 +127,20 @@
                                 required />
                         </div>
 
-                        <div class="w-full">
+                        <div class="w-full mr-4">
                             <label class="label">
                                 <span class="label-text">É Eleitor</span>
                             </label>
                             <input class="input input-bordered w-full  rounded-xl" type="text" v-model="form.eleitor"
                                 required />
                         </div>
-                    </div>
-
-                    <div>
+                        <div class="w-full">
                         <label class="label">
                             <span class="label-text">Carteira Sindical</span>
                         </label>
                         <input class="input input-bordered w-full  rounded-xl" type="text" v-model="form.eleitor"
                             required />
+                    </div>
                     </div>
 
                     <div class="flex w-full">
@@ -182,16 +170,14 @@
 </template>
   
 <script>
+import Button from '../components/Button.vue';
 import { ref } from 'vue';
-import Camera from 'simple-vue-camera';
 
 export default {
     data() {
         return {
-            snapshotUrl: null,
-            showCameraModal: false,
-            showImageModal: false,
-            selectedImageUrl: null,
+            selectedImage: null,
+            isCameraActive: false,
             form: {
                 imagem: '',
                 nome: '',
@@ -211,7 +197,6 @@ export default {
         const takeSnapshot = async () => {
             const blob = await camera.value?.snapshot();
 
-            // Se a foto foi capturada, converta para base64 e atribua a `imagem` no formulário
             if (blob) {
                 const reader = new FileReader();
                 reader.readAsDataURL(blob);
@@ -222,7 +207,6 @@ export default {
         };
 
         const submitForm = () => {
-            // Implemente a lógica para enviar o formulário aqui
             console.log('Formulário enviado:', this.form);
         };
 
@@ -233,30 +217,66 @@ export default {
         };
     },
     methods: {
-    openCamera() {
-      this.showCameraModal = true;
+        addDragOverClass() {
+            this.$refs.dropArea.classList.add('drag-over');
+        },
+        removeDragOverClass() {
+            this.$refs.dropArea.classList.remove('drag-over');
+        },
+        clearSelection() {
+            this.selectedImage = null;
+        },
+        handleDrop(event) {
+            this.removeDragOverClass();
+
+            const files = event.dataTransfer.files;
+            if (files.length > 0) {
+                this.handleImage(files[0]);
+            }
+        },
+        openFileInput() {
+            const input = document.createElement('input');
+            input.type = 'file';
+            input.accept = 'image/*';
+            input.onchange = (event) => {
+                const file = event.target.files[0];
+                if (file) {
+                    this.handleImage(file);
+                }
+            };
+            input.click();
+        },
+        openCamera() {
+            this.isCameraActive = true;
+            // Adicione a lógica específica da câmera aqui, por exemplo, iniciar a câmera.
+            navigator.mediaDevices
+                .getUserMedia({ video: true })
+                .then((stream) => {
+                    this.$refs.cameraFeed.srcObject = stream;
+                })
+                .catch((error) => {
+                    console.error('Erro ao acessar a câmera:', error);
+                    this.isCameraActive = false;
+                });
+        },
+        captureFromCamera() {
+            // Adicione a lógica para capturar uma imagem da câmera aqui.
+            const canvas = document.createElement('canvas');
+            canvas.width = this.$refs.cameraFeed.videoWidth;
+            canvas.height = this.$refs.cameraFeed.videoHeight;
+            const context = canvas.getContext('2d');
+            context.drawImage(this.$refs.cameraFeed, 0, 0, canvas.width, canvas.height);
+
+            // Converta o quadro capturado para uma imagem base64 (data URL)
+            const imageData = canvas.toDataURL('image/png');
+            console.log('Imagem da câmera capturada:', imageData);
+
+            // Aqui, você pode definir this.selectedImage ou realizar qualquer ação desejada com a imagem capturada.
+
+            // Desligue a câmera após a captura
+            this.isCameraActive = false;
+        },
     },
-    closeCameraModal() {
-      this.showCameraModal = false;
-    },
-    openImageModal() {
-      this.showImageModal = true;
-    },
-    closeImageModal() {
-      this.showImageModal = false;
-    },
-    takeSnapshot() {
-      // Lógica para capturar uma foto da câmera
-    },
-    handleFileInput(event) {
-      // Lógica para lidar com a seleção de um arquivo do computador
-      const file = event.target.files[0];
-      if (file) {
-        this.selectedImageUrl = URL.createObjectURL(file);
-        this.openImageModal();
-      }
-    },
-  },
 };
 </script>
 
