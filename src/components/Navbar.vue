9a5b4ee4f1ace991835f9c62b7bab4fe50fa5666
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-19 21:20:45 -0300

    :sparkles: feat add  Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,22 @@
 <template>
-    <nav class=" h-screen w-20 bg-white">
+    <nav class="h-screen w-24 flex flex-col justify-between p-2 bg-secondary">
+        <div>
+            <img src="" alt="">
+        </div>
+        
+        <ul class="flex flex-col h-1/4 justify-between">
+            <li><Button @click="$router.push('/')" class="w-full"><img class="w-full" src="../assets/imgs/icons/search.svg" alt=""></Button></li>
+            <li><Button @click="$router.push('/create')" class="w-full"><img class="w-full" src="../assets/imgs/icons/plus.svg" alt=""></Button></li>
+            <li><Button class="w-full"><img class="w-full" src="../assets/imgs/icons/calendar.svg" alt=""></Button></li>
+        </ul>
+        <Button class="w-full btn-ghost"><img class="w-full" src="../assets/imgs/icons/exit.svg" alt=""></Button>
     </nav>
 </template>
+<script>
+import Button from './Button.vue';
+export default {
+    components: {
+        Button
+    }
+}
+</script>
