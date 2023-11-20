25686fb88025b06915699c99db2dadc8241089b6
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-19 21:22:49 -0300

    Add tailwind utility class for button styling

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import router from './router'
+import Camera from 'simple-vue-camera'
 
 const app = createApp(App)
 app.use(router)
+app.component('camera', Camera) // Registre o componente globalmente
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../pages/Home.vue'
+import Create from '../pages/Create.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
   },
 ]
 
