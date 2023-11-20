19875ab9ca4f91588ccb2651748684f96a635416
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-19 21:19:54 -0300

    :sparkles: feat Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<!-- Button.vue -->
+<template>
+    <button class="btn sm:btn-sm md:btn-md lg:btn-lg" @click="handleClick">
+      <slot></slot>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      handleClick() {
+        this.$emit('click');
+      }
+    }
+  };
+  </script>
+  
