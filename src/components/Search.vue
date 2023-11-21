d0e25c3d3d21ca841a43a9458812d6e6df37ee3b
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-21 20:40:40 -0300

    :sparkles: 	feat Add Search component with input and button

---
+++ src/components/Search.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-80 h-16 flex">
+        <input type="text" placeholder="Buscar" class="input h-full input-bordered w-72 max-w-xs rounded-xl mr-2" />
+        <Button class="btn btn-primary h-full rounded-xl">Buscar</Button>
+    </div>
+</template>
+<script>
+import Button from './Button.vue';
+</script>
