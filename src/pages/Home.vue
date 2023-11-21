c5960c2bdbe3ad72f28d40070e4167af05a6b722
Thiago Gentil <thiagogentil499@gmail.com>
2023-11-21 20:49:11 -0300

    :recycle: refactor Home.vue layout and components

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-full p-8">
-        <header>
-            <Searchbar />
+    <div class="w-full px-20">
+        <header class="fixed w-full h-28 pb-2 flex items-end z-10 top-0 bg-base-100">
+            <Search></Search>
         </header>
-        <main>
-            <table class="table-lg w-1/2 mt-12">
+        <main class="mt-32 flex">
+            <table class="table-xs lg:table-md w-1/2 mt-12">
                 <!-- head -->
                 <thead>
                     <tr>
@@ -15,14 +15,14 @@
                 </thead>
                 <tbody>
                     <!-- row 1 -->
-                    <tr @click="" class="hover:bg-neutral cursor-pointer">
+                    <tr v-for="index in 100" :key="index" @click="" class="cursor-pointer p-8 border-b-2 border-neutral hover:bg-base-300">
                         <td class="avatar">
                             <div class="mask mask-squircle w-20 h-20">
                                 <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                     alt="Avatar Tailwind CSS Component" />
                             </div>
                         </td>
-                        <td>
+                        <td class="">
                             <div class="flex items-center gap-3">
                                 <div>
                                     <div class="font-bold text-2xl">Hart Hagerty</div>
@@ -35,16 +35,25 @@
                         </td>
                     </tr>
                     
+                    
                 </tbody>
             </table>
+            <div class="w-1/2 h-full fixed top-0 right-0 flex items-center justify-center">
+                <div class="w-5/6 h-5/6 mt-16 bg-base-300 rounded-2xl">
+                    <img src="" alt="">
+                    <h1></h1>
+                    
+                </div>
+            </div>
         </main>
     </div>
 </template>
 <script>
-import Searchbar from '../components/Searchbar.vue';
+import Search from '../components/Search.vue';
 export default {
+    name: 'Home',
     components: {
-        Searchbar
+        Search
     }
 }
 </script>
