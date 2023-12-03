d88266fd981ee95c78436050391de717e2acbffa
Thiago Gentil <thiagogentil499@gmail.com>
2023-12-03 20:33:37 -0300

    :sparkles: feat  login route and authentication check

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Search from '../pages/Search.vue'
-import Create from '../pages/Create.vue'
-import Monthly from '../pages/Monthly.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../pages/Home.vue';
+import Search from '../pages/Search.vue';
+import Create from '../pages/Create.vue';
+import Monthly from '../pages/Monthly.vue';
+import Login from '../pages/Login.vue';
+import { isAuthenticated } from '../services/auth.ts';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -18,18 +20,38 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/create',
     name: 'Create',
-    component: Create
+    component: Create,
+    meta: { requiresAuth: true }  // Esta rota requer autenticação
   },
   {
     path: '/monthly',
     name: 'Monthly',
-    component: Monthly
+    component: Monthly,
+    meta: { requiresAuth: true }  // Esta rota requer autenticação
   },
-]
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const isAuthenticatedUser = isAuthenticated();
+    if (!isAuthenticatedUser) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
