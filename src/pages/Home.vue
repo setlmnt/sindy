e52a55af462b552e065a4ca94d1ea66b2567fd33
Thiago Gentil <thiagogentil499@gmail.com>
2023-12-20 01:12:21 -0300

    Add data fetching and display for total associates and birthday associates

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,42 @@
 <template>
-    
+    <div class="p-20 w-full min-h-screen">
+        <header class="w-full h-8">
+            <h1 class="font-bold text-5xl">Visão Geral</h1>
+        </header>
+        <main class="w-2/3 flex justify-between mt-20">
+            <div class="card w-1/3 mr-4 bg-primary text-primary-content">
+                <div class="card-body">
+                    <h2 class="card-title">Total de Associados Cadastrados</h2>
+                    <p class="font-bold text-4xl">{{ this.totalPersons }}</p>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
+<script>
+import { getAllAssociates, getAllBirthdayAssociates } from '../api/associatesApi.ts';
+
+export default {
+    data() {
+        return {
+            isLoading: true,
+            totalPersons:'',
+            birthdayAssociates: [],
+        };
+    },
+    mounted() {
+        this.loadData();
+        this.loadBirthdayAssociates();
+    },
+    methods: {
+        async loadData() {
+               const response = await getAllAssociates();
+                this.totalPersons = response.totalElements;
+        },
+        async loadBirthdayAssociates() {
+            const response = await getAllBirthdayAssociates();
+            this.birthdayAssociates = response.content;
+        },
+    }
+}
+</script>
